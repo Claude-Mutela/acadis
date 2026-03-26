dacde69cf5865e3b80a8e02d8354433c5ca290c9
Claude Mutela <claudemutela9@gmail.com>
2026-03-26 20:45:38 +0100

    refactor(inertia): désactiver le wrapper de layout par défaut global
    - Suppression de l'application automatique de [layouts/default.tsx](cci:7://file:///c:/_dev360/H26/projects/acadis/inertia/layouts/default.tsx:0:0-0:0)
    - Nettoyage des imports React obsolètes (Data, ReactElement, Layout)
    - Permet l'usage exclusif du Layout personnalisé par page

--- inertia/app.tsx
+++ inertia/app.tsx
@@ -1,8 +1,5 @@
 import './css/app.css'
-import { ReactElement } from 'react'
 import { client } from './client'
-import Layout from '~/layouts/default'
-import { Data } from '@generated/data'
 import { createRoot } from 'react-dom/client'
 import { createInertiaApp } from '@inertiajs/react'
 import { TuyauProvider } from '@adonisjs/inertia/react'
@@ -16,7 +13,6 @@ createInertiaApp({
     return resolvePageComponent(
       `./pages/${name}.tsx`,
       import.meta.glob('./pages/**/*.tsx'),
-      (page: ReactElement<Data.SharedProps>) => <Layout children={page} />
     )
   },
   setup({ el, App, props }) {
