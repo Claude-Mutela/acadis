a976e8422f27aa7d50f82c35701b3029e0db0f39
Claude Mutela <claudemutela9@gmail.com>
2026-04-08 23:36:46 +0100

    modele(user) create user model

--- app/models/user.ts
+++ app/models/user.ts
@@ -3,12 +3,23 @@ import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 
+import type { HasOne } from '@adonisjs/lucid/types/relations'
+import { hasOne, column } from '@adonisjs/lucid/orm'
+import UserProfile from './user_profile.js'
+
 export default class User extends compose(UserSchema, withAuthFinder(hash)) {
+  
+  @column()
+  declare role: 'superadmin' | 'admin' | 'student' | 'trainer'
+
+  @column()
+  declare status: 'active' | 'inactive' | 'suspended'
+
+  @hasOne(() => UserProfile)
+  declare profile: HasOne<typeof UserProfile>
+
   get initials() {
-    const [first, last] = this.fullName ? this.fullName.split(' ') : this.email.split('@')
-    if (first && last) {
-      return `${first.charAt(0)}${last.charAt(0)}`.toUpperCase()
-    }
-    return `${first.slice(0, 2)}`.toUpperCase()
+    const defaultInitials = this.email.substring(0, 2).toUpperCase()
+    return defaultInitials
   }
 }
