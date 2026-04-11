577e4be374a31f3ed578660913022c36d59064c2
Claude Mutela <claudemutela9@gmail.com>
2026-04-11 02:07:40 +0100

    feat(routes): implémentation des routes inertielles /galerie et /galerie/:slug

--- start/routes.ts
+++ start/routes.ts
@@ -14,6 +14,10 @@ import router from '@adonisjs/core/services/router'
 router.on('/').renderInertia('home', {}).as('home')
 router.on('/about').renderInertia('about', {}).as('about')
 router.on('/programme').renderInertia('programme', {}).as('programme')
+router.on('/galerie').renderInertia('galerie/index', {}).as('galerie')
+router.get('/galerie/:slug', async ({ params, inertia }) => {
+  return inertia.render('galerie/album', { slug: params.slug })
+}).as('galerie.show')
 router.get('/programme/:id', async ({ params, inertia }) => {
   return inertia.render('programme_detail', { id: params.id })
 }).as('programme.show')
